afea15a40bd5ff258ddf4477a6d6d2bb4cc34733
MateusKuzanagi <mateusassis42@gmail.com>
2026-07-15 16:38:19 -0300

    app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def init_db():
         KMEntrada TEXT, KMSaida TEXT, DataEntrada TEXT, DataSaida TEXT
     )""")
 
-    # Criação da tabela de Vendas/OS incluindo as colunas de controle extra
+    # Criação da tabela de Vendas/OS incluindo as novas colunas
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Vendas(
         ID INTEGER PRIMARY KEY AUTOINCREMENT, 
